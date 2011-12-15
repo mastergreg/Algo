4c98938aed289473d52fd57951675bab8b3e00b8
Gregory Liras <gregliras@gmail.com>
2011-12-15 14:21:08 +0200

    vacation written clearer

--- Programming/Ask_1/cpp/vacation/vacation.cpp
+++ Programming/Ask_1/cpp/vacation/vacation.cpp
@@ -6,7 +6,7 @@
 
 * Creation Date : 28-11-2011
 
-* Last Modified : Sun 11 Dec 2011 01:44:37 AM EET
+* Last Modified : Thu 15 Dec 2011 02:19:50 PM EET
 
 * Created By : Greg Liras <gregliras@gmail.com>
 
@@ -18,53 +18,58 @@ _._._._._._._._._._._._._._._._._._._._._.*/
 #include <numeric>
 #include <cmath>
 
+#ifndef MAXA 
+#define MAXA 500000
+#endif
 using namespace std;
 
-class touple
-{
-    public:
-        int sum;
-        int index;
-        touple(int s,int i);
-};
-int solve(vector<touple *> lefts,vector<touple *> rights);
-vector<touple *> getlefts(vector<int> sums);
-vector<touple *> getrights(vector<int> sums);
+int solve(vector<int> lefts,vector<int> rights,vector<int> sums);
+int getlefts(vector<int>::iterator it,vector<int> sums);
+int getrights(vector<int>::iterator it,vector<int> sums);
 
-touple::touple(int s,int i)
-{
-    sum=s;
-    index=i;
-}
 int main()
 {
     unsigned int days=0;
     unsigned int i=0;
     int min_temp=0;
     int max_days=0;
+    int cr=0;
+    int cl=0;
     vector<int> temperatures;
     vector<int> prefix_sums;
-    vector<touple *> lefts;
-    vector<touple *> rights;
+    vector<int> lefts;
+    vector<int> rights;
 
 
     cin >> days >> min_temp;
     temperatures.resize(days);
     prefix_sums.resize(days);
-    for (i=0;i<days;i++)
+    lefts.resize(days);
+    rights.resize(days);
+
+    cin >> temperatures[0];
+    temperatures[0]-=min_temp;
+    prefix_sums[0]=temperatures[0];
+    for (i=1;i<days;i++)
     {
         cin >> temperatures[i];
         temperatures[i]-=min_temp;
+        prefix_sums[i]=prefix_sums[i-1]+temperatures[i];
     }
-    partial_sum(temperatures.begin(),temperatures.end(),prefix_sums.begin());
-    lefts=getlefts(prefix_sums);
-    rights=getrights(prefix_sums);
-    max_days = solve(lefts,rights);
+    //partial_sum(temperatures.begin(),temperatures.end(),prefix_sums.begin());
+    cl=getlefts(lefts.begin(),prefix_sums);
+    lefts.erase(lefts.begin()+cl,lefts.end());
+    //lefts.resize(cr);
+
+    cr=getrights(rights.begin(),prefix_sums);
+    rights.erase(rights.begin()+cl,rights.end());
+    //rights.resize(cr);
+    max_days = solve(lefts,rights,prefix_sums);
     max_days = min(max_days,(int)days);
     cout << max_days << endl;
     return 0;
 }
-int solve(vector<touple *> lefts,vector<touple *> rights)
+int solve(vector<int> lefts,vector<int> rights,vector<int> sums)
 {
     int i=0;
     unsigned int j=0;
@@ -75,48 +80,58 @@ int solve(vector<touple *> lefts,vector<touple *> rights)
     limitj = rights.size();
     for(i=limiti-1,j=0;j<limitj;j++)
     {
+        if(sums[rights[j]]>=0)
+        {
+            buf=max(buf,rights[j]+1);
+        }
         for(;i>=0;i--)
         {
-            if(((rights[j]->sum)-(lefts[i]->sum))>0)
-                buf=max(buf,(rights[j]->index)-(lefts[i]->index));
+            if((sums[rights[j]]-sums[lefts[i]])>=0)
+                buf=max(buf,rights[j]-lefts[i]);
             else
                 break;
         }
     }
     return buf;
 }
-vector<touple *> getlefts(vector<int> sums)
+int getlefts(vector<int>::iterator it, vector<int> sums)
 {
-    vector<touple *> lefts;
     int i;
     int lim=sums.size();
     int curr=sums[0];
-    lefts.push_back(new touple(0,-1));
-    for(i=0;i<lim;i++)
+    *it=0;
+    it++;
+    int c =1;
+    for(i=1;i<lim;i++)
     {
         if(sums[i]<curr)
         {
             curr=sums[i];
-            lefts.push_back(new touple(sums[i],i));
+            (*it)=i;
+            it++;
+            c++;
         }
     }
-    return lefts;
+    return c;
 }
-vector<touple *> getrights(vector<int> sums)
+int getrights(vector<int>::iterator it, vector<int> sums)
 {
-    vector<touple *> rights;
     int i;
     int lim=sums.size()-1;
     int curr=0;
-    rights.push_back(new touple(sums[lim],lim+1));
+    int c =1;
+    *it=lim;
+    it++;
     curr=sums[lim];
     for(i=lim-1;i>=0;i--)
     {
         if(sums[i]>curr)
         {
             curr=sums[i];
-            rights.push_back(new touple(sums[i],i));
+            *it=i;
+            it++;
+            c++;
         }
     }
-    return rights;
+    return c;
 }
