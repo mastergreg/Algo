3abaf0db13c068b1accdd47012587927ada8b1a8
Gregory Liras <gregliras@gmail.com>
2011-12-19 17:54:37 +0200

    added testcases for second programming ex

---
+++ Programming/Ask_2/cpp/teleport/teleport.cpp
@@ -0,0 +1,13 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+
+* File Name : teleport.cpp
+
+* Purpose :
+
+* Creation Date : 19-12-2011
+
+* Last Modified : Mon 19 Dec 2011 05:51:29 PM EET
+
+* Created By : Greg Liras <gregliras@gmail.com>
+
+_._._._._._._._._._._._._._._._._._._._._.*/
---
+++ Programming/Ask_2/cpp/xmasshopping/xmasshopping.cpp
@@ -0,0 +1,13 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+
+* File Name : xmasshopping.cpp
+
+* Purpose :
+
+* Creation Date : 19-12-2011
+
+* Last Modified : Mon 19 Dec 2011 05:51:15 PM EET
+
+* Created By : Greg Liras <gregliras@gmail.com>
+
+_._._._._._._._._._._._._._._._._._._._._.*/
