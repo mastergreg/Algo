a1765f0e8f65fd4a95fb90683a909aaf1737fd72
Gregory Liras <gregliras@gmail.com>
2011-12-28 03:05:26 +0200

    starting programming

--- Programming/Ask_2/cpp/teleport/teleport.cpp
+++ Programming/Ask_2/cpp/teleport/teleport.cpp
@@ -6,8 +6,68 @@
 
 * Creation Date : 19-12-2011
 
-* Last Modified : Mon 19 Dec 2011 05:51:29 PM EET
+* Last Modified : Tue 27 Dec 2011 07:52:26 PM EET
 
 * Created By : Greg Liras <gregliras@gmail.com>
 
 _._._._._._._._._._._._._._._._._._._._._.*/
+
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
+#include <algorithm>
+using namespace std;
+bool compare(vector<int> v1,vector <int> v2)
+{
+    return v1[0] < v2[0] || (v1[0]==v2[0] && v1[1] < v2[1]);
+}
+bool compare2(vector<int> v1,vector <int> v2)
+{
+    return v1[1] < v2[1] || (v1[1]==v2[1] && v1[0] < v2[0]);
+}
+bool incomp(vector<int> v1, vector<int> v2)
+{
+    return v2[1]<v1[1] && v2[0] > v1[0];
+}
+bool compatible(vector<int> v1, vector<int> v2)
+{
+    return v2[1]>=v1[1] || v2[0] <= v1[0];
+}
+int main()
+{
+    int scientists=0;
+    int i;
+    int nothing;
+    vector< vector < int > > AB;
+    vector< vector < int > > ABcopy;
+    //get how many are there
+
+    nothing = scanf("%d",&scientists);
+
+    AB.reserve(scientists);
+    ABcopy.resize(scientists);
+
+    for (i=0;i<scientists;i++)
+    {
+        AB.push_back(vector <int> (2));
+        nothing = scanf("%d %d",&AB[i][0],&AB[i][1]);
+    }
+
+
+    for (i=0;i<scientists;i++)
+    {
+        printf("%d %d\n",AB[i][0],AB[i][1]);
+    }
+    copy(AB.begin(),AB.end(),ABcopy.begin());
+    sort(AB.begin(),AB.end(),compare);
+    sort(ABcopy.begin(),ABcopy.end(),compare2);
+    printf("\n");
+    for (i=0;i<scientists;i++)
+    {
+        printf("%d %d \t\t %d %d\n",AB[i][0],AB[i][1],ABcopy[i][0],ABcopy[i][1]);
+    }
+
+
+    
+
+}
--- Programming/Ask_2/cpp/xmasshopping/xmasshopping.cpp
+++ Programming/Ask_2/cpp/xmasshopping/xmasshopping.cpp
@@ -6,8 +6,43 @@
 
 * Creation Date : 19-12-2011
 
-* Last Modified : Mon 19 Dec 2011 05:51:15 PM EET
+* Last Modified : Tue 27 Dec 2011 11:40:39 PM EET
 
 * Created By : Greg Liras <gregliras@gmail.com>
 
 _._._._._._._._._._._._._._._._._._._._._.*/
+
+
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main(void)
+{
+    int n=0;
+    int r=0;
+    int c=0;
+    int nothing;
+    int i;
+    vector< vector < int > > shops;
+    vector< int > XY1(2,0);
+    vector< int > XY2(2,0);
+
+
+
+
+    nothing = scanf("%d %d %d",&n,&r,&c);
+    shops.reserve(n);
+
+    nothing = scanf("%d %d",&XY1[0],&XY1[1]);
+    nothing = scanf("%d %d",&XY2[0],&XY2[1]);
+    for (i=0;i<n;i++)
+    {
+        shops.push_back(vector <int> (2));
+        nothing = scanf("%d %d",&shops[i][0],&shops[i][1]);
+    }
+
+}
