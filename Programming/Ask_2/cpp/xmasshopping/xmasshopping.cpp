95220e6d2bcc4d0c18cc95e6fcb386ea0d0eaa46
Gregory Liras <gregliras@gmail.com>
2012-01-02 21:40:19 +0200

    need to cut down on space

--- Programming/Ask_2/cpp/xmasshopping/xmasshopping.cpp
+++ Programming/Ask_2/cpp/xmasshopping/xmasshopping.cpp
@@ -6,7 +6,7 @@
 
 * Creation Date : 19-12-2011
 
-* Last Modified : Mon 02 Jan 2012 06:13:05 PM EET
+* Last Modified : Mon 02 Jan 2012 09:36:44 PM EET
 
 * Created By : Greg Liras <gregliras@gmail.com>
 
@@ -21,14 +21,8 @@ _._._._._._._._._._._._._._._._._._._._._.*/
 
 using namespace std;
 
-/* needs to be ancestor matrix */
-struct node
-{
-    int index;
-    int father;
-    int cost;
-};
 
+inline
 int manhatan(const vector<int>& v1,const vector<int>& v2)
 {
     /* =========================== 
@@ -38,78 +32,41 @@ int manhatan(const vector<int>& v1,const vector<int>& v2)
     return abs(v1[0]-v2[0])+abs(v1[1]-v2[1]);
 }
 
-void fill_line(int j,vector<int>& line1,vector<int>& line2,vector< vector<int> >& distances,vector< vector<int> >& shops) 
-{
-    int i;
-    int prev;
-    for (i=2;i<line2.size();i++)
-    {
-        /* ===============================
-         * I need the previous 
-         * consecutive node of i,j
-         * which will be either i=1 or j-1
-         * =============================== */
-        prev=min(i,j)-1;
-        if(distances[prev][i]==-1)
-        {
-            distances[prev][i]=manhatan(shops[prev],shops[i]);
-        }
-        if(distances[prev][j]==-1)
-        {
-            distances[prev][j]=manhatan(shops[prev],shops[j]);
-        }
-        line2[i]=min(distances[prev][i]+line2[prev],distances[prev][j]+line1[prev]);
-    }
-
-}
-
-int lesser(vector< vector<int> >& distances,vector< vector<int> >& shops)
-{
-    int i,j,z;
-    bool flag=true;
-    int n = shops.size();
-    vector< vector<int> > lines(n,vector<int>(n,0));
-    
-    for(i=0;i<n;i++)
-    {
-        for (j=0;j<n;j++)
-        {
-            distances[i][j]=manhatan(shops[i],shops[j]);
-        }
-    }
-    for(i=0;i<n;i++)
-    {
-        for (j=0;j<n;j++)
-        {
-            //lines[i][j]=min(distances[i-1]
-        }
-    }
-
-
-}
-
-int less(int i ,int j ,vector< vector<int> >& distances,vector< vector<int> >& shops,vector< vector<int> >& lessmatrix)
+int less(int n,vector< vector<int> >& shops)
 {
     int next;
-    if(j==shops.size()-1 || i==shops.size()-1)
+    int distances_i_next,distances_j_next;
+    int **lessmatrix = new int*[n];
+    for(int i=0;i<n;i++)
     {
-        return 0;
+        lessmatrix[i]=new int[n];
     }
-    next=max(i,j)+1;
-    if(distances[i][next]==-1)
+    lessmatrix[n-1][n-1]=0;
+    //if(j==n-1 || i==n-1)
+    //{
+    //    return 0;
+    //}
+    //next=max(i,j)+1;
+    //    distances_i_next=manhatan(shops[next],shops[i]);
+    //    distances_j_next=manhatan(shops[next],shops[j]);
+    //if(lessmatrix[i][j]==-1)
+    //{
+    //    lessmatrix[i][j]= min(distances_i_next+less(next,j,n,shops,lessmatrix),
+    //            distances_j_next+less(next,i,n,shops,lessmatrix));
+    //}
+    for(int i=n-1;i>=0;i--)
     {
-        distances[i][next]=manhatan(shops[next],shops[i]);
+        for(int j=n-1;j>=0;j--)
+        {
+            next=min(n-1,max(i,j)+1);
+            distances_i_next=manhatan(shops[next],shops[i]);
+            distances_j_next=manhatan(shops[next],shops[j]);
+            lessmatrix[i][j]= min(distances_i_next+lessmatrix[next][j],
+                    distances_j_next+lessmatrix[next][i]);
+        }
+
     }
-    if(distances[j][next]==-1)
-    {
-        distances[j][next]=manhatan(shops[next],shops[j]);
-    }
-    if(lessmatrix[i][j]==-1)
-    {
-        lessmatrix[i][j]= min(distances[i][next]+less(next,j,distances,shops,lessmatrix),
-                distances[j][next]+less(next,i,distances,shops,lessmatrix));
-    }
-    return lessmatrix[i][j];
+    return lessmatrix[0][1];
 }
 
 int main(void)
@@ -120,11 +77,9 @@ int main(void)
     int nothing;
     int i;
     vector< vector < int > > shops;
-    vector< int > s1(2,0);
-    vector< int > s2(2,0);
+    //int **shops;
     /* ==== GET INPUT ==== */
     nothing = scanf("%d %d %d",&n,&r,&c);
-    vector< vector < int > > distances(n+2,vector<int>(n+2,-1));
     vector< vector < int > > lessmatrix(n+2,vector<int>(n+2,-1));
     lessmatrix[n-1][n-1]=0;
     shops.reserve(n+2);
@@ -137,17 +92,9 @@ int main(void)
         nothing = scanf("%d %d",&shops[i][0],&shops[i][1]);
         //ugly ugliness of ugly
     }
-    s1=shops[0];
-    s2=shops[1];
-    /* ==== GET INPUT ==== */
-    //generate_all_distances(shops,distances);
-    //gen_mdists(shops,mdists);
-    
-    /* ==== prefix_sums of manhatan distances ==== */
-    //partial_sum(mdists.begin(),mdists.end(),prefix_manthatan_distance_sums.begin()+1);
 
     //printf("%d\n",solveMe(s1,s2,shops,prefix_manthatan_distance_sums));
-    printf("%d\n",less(0,1,distances,shops,lessmatrix));
+    printf("%d\n",less(n+2,shops));
     //printf("%d\n",lesser(distances,shops));
 
 
