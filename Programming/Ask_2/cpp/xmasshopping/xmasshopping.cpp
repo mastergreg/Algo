e3f66a951e4c4e17644329f3989f33579428d279
Gregory Liras <gregliras@gmail.com>
2012-01-02 23:26:50 +0200

    spacing

--- Programming/Ask_2/cpp/xmasshopping/xmasshopping.cpp
+++ Programming/Ask_2/cpp/xmasshopping/xmasshopping.cpp
@@ -6,7 +6,7 @@
 
 * Creation Date : 19-12-2011
 
-* Last Modified : Mon 02 Jan 2012 10:55:40 PM EET
+* Last Modified : Mon 02 Jan 2012 11:26:17 PM EET
 
 * Created By : Greg Liras <gregliras@gmail.com>
 
@@ -31,52 +31,51 @@ int manhatan(const int *v1,const int *v2)
      * Calculate manthatan 
      * distance of two points
      * =========================== */
-    return abs(v1[0]-v2[0])+abs(v1[1]-v2[1]);
+    return abs( v1[0] - v2[0] ) + abs( v1[1] - v2[1] );
 }
 
 int solveMe(int n,int **shops)
 {
     int next;
-    int distances_i_next,distances_j_next;
+    int distances_i_next;
+    int distances_j_next;
     int *current_line = new int[n];
     int *prev_line = new int[n];
-    prev_line[n-1]=0;
+    prev_line[n-1] = 0;
 
-    for(int i=n-1;i>=0;i--)
+    for(int i = n-1 ; i >= 0 ; i--)
     {
-        next=min(n-1,i+1);
-        distances_i_next=manhatan(shops[next],shops[i]);
-        for(int j=i-1;j>=0;j--)
+        next = min( n-1 , i+1 );
+        distances_i_next = manhatan( shops[next] , shops[i] );
+        for(int j = i-1 ; j >= 0 ; j--)
         {
-            distances_j_next=manhatan(shops[next],shops[j]);
-            current_line[j]= min(distances_i_next+prev_line[j],
-                    distances_j_next+prev_line[i]);
+            distances_j_next = manhatan( shops[next] , shops[j] );
+            current_line[j] = min( distances_i_next + prev_line[j],
+                    distances_j_next + prev_line[i] );
         }
-        swap(current_line,prev_line);
+        swap( current_line , prev_line );
     }
     return current_line[0];
 }
 
 int main(void)
 {
-    int n=0;
-    int r=0;
-    int c=0;
+    int n = 0;
+    int r = 0;
+    int c = 0;
     int nothing;
     int **shops;
     /* ==== GET INPUT ==== */
-    nothing = scanf("%d %d %d",&n,&r,&c);
-    n+=2;
+    nothing = scanf("%d %d %d" , &n , &r , &c);
+    n += 2;
 
     shops = new int*[n];
-    for (int i=0;i<n;i++)
+    for (int i = 0 ; i < n ; i++)
     {
-        shops[i]=new int[2];
-        nothing = scanf("%d %d",&shops[i][0],&shops[i][1]);
+        shops[i] = new int[2];
+        nothing = scanf("%d %d" , &shops[i][0] , &shops[i][1]);
     }
-    printf("%d\n",solveMe(n,shops));
-
+    printf( "%d\n" , solveMe( n , shops ) );
     return 0;
-
 }
 
