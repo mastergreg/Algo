87ce0bda3805edbbf998753239e0e56cc83ad91a
Gregory Liras <gregliras@gmail.com>
2012-01-07 00:14:31 +0200

    added makis

--- Programming/Ask_2/cpp/teleport/teleport.cpp
+++ Programming/Ask_2/cpp/teleport/teleport.cpp
@@ -6,7 +6,7 @@
 
  * Creation Date : 19-12-2011
 
- * Last Modified : Fri 06 Jan 2012 04:53:11 PM EET
+ * Last Modified : Sat 07 Jan 2012 12:13:49 AM EET
 
  * Created By : Greg Liras <gregliras@gmail.com>
 
@@ -80,205 +80,9 @@ void clean_overlapping(list< pair < int , int > >& alist)
     }
 }
 
-inline
-bool collidesA(pair<int,int>& rights_elem,pair<int,int>& lefts_elem)
-{
-    /*
-     * [ -> ]       = rights_elem
-     *    [ <- ]    = lefts_elem
-     */
-    return (rights_elem.second > lefts_elem.second && \
-            rights_elem.second < lefts_elem.first);
-
-}
-
-inline
-bool collidesB(pair<int,int>& rights_elem,pair<int,int>& lefts_elem)
-{
-    /*
-     *    [ -> ]    = rights_elem
-     * [ <- ]       = lefts_elem
-     */
-    return (rights_elem.first < lefts_elem.first && \
-            rights_elem.second > lefts_elem.first);
-}
-
-inline
-bool collidesC(pair<int,int>& rights_elem,pair<int,int>& lefts_elem)
-{
-    /*
-     *    [ -> ]    = rights_elem
-     * [    <-   ]  = lefts_elem
-     */
-    return (rights_elem.first > lefts_elem.second && \
-            rights_elem.second < lefts_elem.first);
-}
-
-inline
-bool collidesD(pair<int,int>& rights_elem,pair<int,int>& lefts_elem)
-{
-    /*
-     * [    ->    ] = rights_elem
-     *    [ <- ]    = lefts_elem
-     */
-    return (rights_elem.first > lefts_elem.second && \
-            rights_elem.second > lefts_elem.first);
-}
-
-inline
-bool noCollision(pair<int,int>& rights_elem,pair<int,int>& lefts_elem)
-{
-    /*                                    
-     * [ -> ]         ||        [ -> ]      = rights_elem
-     *        [ <- ]  || [ <- ]             = lefts_elem
-     */
-    return (rights_elem.second <= lefts_elem.second || \
-            rights_elem.first >= lefts_elem.first);
-}
-
 int getMaxAB(list<pair<int,int> >& rights_list,list<pair<int,int> >& lefts_list)
 {
-    int ans=0;
-    list<pair<int,int> >::reverse_iterator rights_iter=rights_list.rbegin();
-    list<pair<int,int> >::reverse_iterator lefts_iter=lefts_list.rbegin();
-    list<pair<int,int> >::reverse_iterator k,m;
-    int cr;
-    int cl;
-    while(rights_iter++ != rights_list.rend() && lefts_iter++ != lefts_list.rend())
-    {
-        /*
-         * first sanity check then collisions
-         * '&&' will shortcircuit if any iterator is out of bounds
-         */
-        while(rights_iter!=rights_list.rend() && lefts_iter!=lefts_list.rend() && \
-                noCollision(*rights_iter,*lefts_iter))
-        {
-            ans++;
-            /*
-             * increase the one 
-             * which is on the left
-             */
-            if(rights_iter->second <= lefts_iter->second)
-            {
-                rights_iter++;
-                cout << "increase right" << endl;
-            }
-            else
-            {
-                lefts_iter++;
-                cout << "increase left" << endl;
-            }
-        }
-        cl=0; 
-        /*
-         * How many times lefts_iter collides
-         */
-        cr=0;
-        /*
-         * How many times rights_iter collides
-         */
-        k=lefts_iter;
-        k++;
-        m=rights_iter;
-        m++;
-
-        /*
-         * Check for type A collisions
-         */
-
-        if(collidesA(*rights_iter,*lefts_iter))
-        {
-            while(k != lefts_list.rend() && collidesA(*rights_iter,*k))
-            {
-                k++;
-                cr++;
-            }
-            while(m != rights_list.rend() && collidesA(*m,*lefts_iter))
-            {
-                m++;
-                cl++;
-            }
-            ans++;
-            /*
-             * Pick the one with the least collisions
-             */
-            if(cl>cr)
-            {
-                rights_iter++;
-                lefts_iter=k;
-            }
-            else
-            {
-                lefts_iter++;
-                rights_iter=m;
-            }
-        }
-
-        /*
-         * Check for type B collisions
-         */
-
-        else if(collidesB(*rights_iter,*lefts_iter))
-        {
-            /*
-             * adding some sanity checking
-             */
-            while(k != lefts_list.rend() && collidesB(*rights_iter,*k))
-            {
-                k++;
-                cr++;
-            }
-            while(m != rights_list.rend() && collidesB(*m,*lefts_iter))
-            {
-                m++;
-                cl++;
-            }
-            ans++;
-            if(cl>cr)
-            {
-                rights_iter++;
-                lefts_iter=k;
-            }
-            else
-            {
-                lefts_iter++;
-                rights_iter=m;
-            }
-        }
-        /*
-         * Check for type C collisions
-         */
-        else if(collidesC(*rights_iter,*lefts_iter))
-        {
-            while(k != lefts_list.rend() && collidesC(*rights_iter,*k))
-            {
-                k++;
-                cr++;
-            }
-            while(m != rights_list.rend() && collidesC(*m,*lefts_iter))
-            {
-                m++;
-                cl++;
-            }
-            ans++;
-            /*
-             * Pick the one with the least collisions
-             */
-            if(cl>cr)
-            {
-                rights_iter++;
-                lefts_iter=k;
-            }
-            }
-            else
-            {
-                lefts_iter++;
-                rights_iter=m;
-            }
-        }
-    }
-
-    return ans;
+    return 0;
 }
 
 int mergeEm(list<pair<int,int> >& rights_list,list<pair<int,int> >& lefts_list)
@@ -293,6 +97,58 @@ void printEm(pair<int,int> & p)
 {
     cout << p.first << "\t" << p.second << endl;
 }
+void mergeTheListsIntoMakis(list< pair<int , int> >& rights_list,
+                            list< pair<int , int> >& lefts_list,
+                            vector<int>& maki)
+{
+    list< pair<int,int> >::iterator rit=rights_list.begin();
+    list< pair<int,int> >::iterator lit=lefts_list.begin();
+
+    int current_right_end;
+    int current_left_end;
+    while(rit!=rights_list.end() && lit!=lefts_list.end())
+    {
+        current_right_end=rit->second;
+        current_left_end=lit->second;
+        if(current_right_end>current_left_end)
+        {
+            if(maki.back()!=current_right_end)
+            {
+                maki.push_back(current_right_end);
+            }
+            rit++;
+
+        }
+        else
+        {
+            if(maki.back()!=current_left_end)
+            {
+                maki.push_back(current_left_end);
+            }
+            lit++;
+        }
+    }
+    /*
+     * Will only get in one of the following
+     */
+    while(rit!=rights_list.end())
+    {
+        if(maki.back()!=current_right_end)
+        {
+            maki.push_back(current_right_end);
+        }
+        rit++;
+    }
+    while(lit!=lefts_list.end())
+    {
+        if(maki.back()!=current_left_end)
+        {
+            maki.push_back(current_left_end);
+        }
+        lit++;
+    }
+}
+
 int solveMe(int **rights,int rightscnt,int ** lefts,int leftscnt)
 {
     /*
@@ -300,6 +156,7 @@ int solveMe(int **rights,int rightscnt,int ** lefts,int leftscnt)
      */
     list< pair <int , int> > rights_list; 
     list< pair <int , int> > lefts_list; 
+    vector<int> maki;
     for(int i=0;i<rightscnt;i++)
         rights_list.push_back(pair<int,int>(rights[i][0],rights[i][1]));
     for(int i=0;i<leftscnt;i++)
@@ -311,6 +168,14 @@ int solveMe(int **rights,int rightscnt,int ** lefts,int leftscnt)
     for_each(rights_list.begin(),rights_list.end(),printEm);
     cout << endl;
     for_each(lefts_list.begin(),lefts_list.end(),printEm);
+
+    maki.reserve(rights_list.size()+lefts_list.size());
+
+
+    mergeTheListsIntoMakis(rights_list,lefts_list,maki);
+
+    
+
     return mergeEm(rights_list,lefts_list);
 }
 
