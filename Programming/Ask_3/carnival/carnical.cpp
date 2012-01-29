07fa9caa1524a7ee44e0dda85a0ee06fee995c46
Gregory Liras <gregliras@gmail.com>
2012-01-29 17:12:38 +0200

    added makefiles and cpp files

---
+++ Programming/Ask_3/carnival/carnical.cpp
@@ -0,0 +1,17 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+
+* File Name : carnical.cpp
+
+* Purpose :
+
+* Creation Date : 29-01-2012
+
+* Last Modified : Sun 29 Jan 2012 05:11:23 PM EET
+
+* Created By : Greg Liras <gregliras@gmail.com>
+
+_._._._._._._._._._._._._._._._._._._._._.*/
+int main()
+{
+    return 0;
+}
---
+++ Programming/Ask_3/headquarters/headquarters.cpp
@@ -0,0 +1,17 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+
+* File Name : headquarters.cpp
+
+* Purpose :
+
+* Creation Date : 29-01-2012
+
+* Last Modified : Sun 29 Jan 2012 05:12:07 PM EET
+
+* Created By : Greg Liras <gregliras@gmail.com>
+
+_._._._._._._._._._._._._._._._._._._._._.*/
+int main()
+{
+    return 0;
+}
