bc6ed4eb559788f170358017e40b58102c5aa7fa
Gregory Liras <gregliras@gmail.com>
2012-02-02 03:11:39 +0200

    need to work on the shifts

--- Programming/Ask_3/headquarters/headquarters.cpp
+++ Programming/Ask_3/headquarters/headquarters.cpp
@@ -6,7 +6,7 @@
 
 * Creation Date : 29-01-2012
 
-* Last Modified : Thu 02 Feb 2012 01:58:56 AM EET
+* Last Modified : Thu 02 Feb 2012 03:11:04 AM EET
 
 * Created By : Greg Liras <gregliras@gmail.com>
 
@@ -23,19 +23,57 @@ using namespace std;
 
 
 
-void multiply(long long int **mat0,long long int **mat2,long long int **mat3,int size)
+void multiply(long long int **mat0,long long int **mat2,long long int **mat3,int N)
 {
     long long int buf;
-    for ( int i=0 ; i < size ; i++ )
+    for ( int i=0 ; i < N ; i++ )
     {
-        for ( int j = 0 ; j < size ; j++ )
+        for ( int j = 0 ; j < N ; j++ )
         {
             buf = 0;
-            for( int k = 0 ; k < size ; k ++)
+            for( int k = 0 ; k < N ; k ++)
             {
                 buf += mat0[i][k]*mat2[k][j];
             }
             mat3[i][j] = buf % MODLIMIT;
+        }
+    }
+}
+
+void solvem(long long int **initial_matrix,long long int **answer, int N, int k)
+{
+    long long int **sup1,**sup2;
+    sup1 = new long long int*[N];
+    sup2 = new long long int*[N];
+    unsigned int bit;
+    for ( int i = 0 ; i < N ; i ++ )
+    {
+        sup1[i] = new long long int[N];
+        copy(initial_matrix[i],initial_matrix[i]+N,sup1[i]);
+        sup2[i] = new long long int[N];
+    }
+    k--;
+    printf("k-1 = %d\n",k);
+    for ( int i = 0 ; i < 32 ; i++)
+    {
+        bit = ( k & ( 1 << i ) ) >> i;
+        if( bit )
+        {
+            printf("2^%d+\n",i);
+            for( int j = 0 ; j < i ; j++)
+            {
+                multiply(sup1,sup1,sup2,N);
+                swap(sup1,sup2);
+            }
+            for ( int i = 0 ; i < N ; i ++ )
+            {
+                for ( int j = 0 ; j < N ; j ++ )
+                {
+                    answer[i][j]+= sup1[i][j];
+                    answer[i][j] %= MODLIMIT;
+                }
+            }
+
         }
     }
 }
@@ -47,7 +85,7 @@ int main()
     int k,N,M,s,t,a,b;
     long long int **mat;
     long long int **mat2;
-    long long int **mat3;
+    //long long int **mat3;
     int log2k;
     int nothing;
 
@@ -56,16 +94,16 @@ int main()
     log2k= (int) log2(k);
     mat = new long long int*[N];
     mat2 = new long long int*[N];
-    mat3 = new long long int*[N];
+    //mat3 = new long long int*[N];
     //adjacency matrix [from][to]
     for( int i = 0 ; i < N ; i ++)
     {
         mat[i] = new long long int[N];
         mat2[i] = new long long int[N];
-        mat3[i] = new long long int[N];
+    //    mat3[i] = new long long int[N];
         fill(mat[i],mat[i]+N,0);
         fill(mat2[i],mat2[i]+N,0);
-        fill(mat3[i],mat3[i]+N,0);
+    //    fill(mat3[i],mat3[i]+N,0);
     }
     for ( int i = 0 ; i < M ; i++ )
     {
@@ -73,19 +111,20 @@ int main()
         a--;
         b--;
         mat[a][b] = 1;
-        mat2[a][b] = 1;
+    //    mat2[a][b] = 1;
     }
-    for( int i = 0 ; i < log2k ; i++)
-    {
-        multiply(mat2,mat2,mat3,N);
-        swap(mat2,mat3);
-    }
-    printf("%d %d\n",(int) pow(2,log2k),k);
-    for ( int i = ( int ) pow(2,log2k); i < k-1 ; i++)
-    {
-        multiply(mat,mat2,mat3,N);
-        swap(mat2,mat3);
-    }
+    //for( int i = 0 ; i < log2k ; i++)
+    //{
+    //    multiply(mat2,mat2,mat3,N);
+    //    swap(mat2,mat3);
+    //}
+    //printf("%d %d\n",(int) pow(2,log2k),k);
+    //for ( int i = ( int ) pow(2,log2k); i < k-1 ; i++)
+    //{
+    //    multiply(mat,mat2,mat3,N);
+    //    swap(mat2,mat3);
+    //}
+    solvem(mat,mat2,N,k);
     printf("%lld\n",mat2[s-1][t-1]);
 
 
